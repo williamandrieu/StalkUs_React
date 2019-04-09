99d4d572cc2a4e6c481bc55b504c86ea12dbcb09
ttanhutau <ttanhutau@gmail.com>
2019-04-09 14:25:28 +0200

    debile je suis

--- src/App.js
+++ src/App.js
@@ -5,17 +5,7 @@ import firebase from './firebase.js';
 
 var db = firebase.firestore();
 
-db.collection("users").add({
-  first: "Ada",
-  last: "Lovelace",
-  born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-  })
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-  })
+
 
 class App extends Component {
   render() {
